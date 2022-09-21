dabe8dcf61d9e115877ac7f74086303e9933330f
Domenico Mancini <mancini.domenico02@gmail.com>
2022-09-21 18:24:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Gamberetti Theme
+A blue and red accent based theme.
---
+++ examples/example.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+
+#define SOMETHING
+
+#ifdef SOMETHING
+#   define SOMETHINGELSE
+#else
+#   define SOMETHINGNOTOK
+#endif
+
+namespace spacename {
+
+}
+
+template <typename T>
+class Templated {
+    T x;
+};
+
+class A {
+
+public:
+
+    static void A();
+    void B();
+};
+
+class Base {
+
+public:
+
+    virtual ~Base() = default;
+};
+
+class Derived : Base {
+
+public:
+
+    int x = 6;
+    int y = 9;
+};
+
+enum class Enumeration {
+    A, B, C, D
+};
+
+template <class T>
+T Sum(T a, T b) {
+    return a + b;
+}
+
+int main(int argc, char** argv){
+    // This is a comment
+
+    /*
+     *  This is also a comment
+     */
+
+    const char* string = "String\n";
+
+    A a = A();
+    a.B();
+
+    A* aa = new A();
+    aa->B();
+
+    Derived d = Derived();
+    d.x = 2;
+
+    int s = Sum<int>(4, 5);
+    main();
+
+    Enumeration e = Enumeration::A;
+
+    float x = .5f * 2;
+
+    std::cout << "Hello World!" << std::endl;
+    std::cin >> x;
+
+    goto LABEL;
+
+    switch (n){
+        case 1:
+            break;
+        default:
+            break;
+    }
+
+    for (int x = 0; x < 5; x++);
+    while(true);
+
+    return 0;
+}
+
+virtual void Strip(const std::string& ref) const override {
+
+}
+
+void Strip(const char* ref) {
+    
+}
